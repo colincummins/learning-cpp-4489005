6c866368b1be99a0f573b69ce33755acc4856759
colincummins <all.our.august.sundays@gmail.com>
2024-09-06 16:17:38 -0700

    Finished exercise 1_2

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,15 @@
 // Learning C++ 
 // Exercise 01_02
-// Hello World, by Eduardo Corpeño 
+// Hello World, by Eduardo Corpeño
+// Modified
+
+#include <iostream>
+
+int main(){
+   std::string str;
+   std::cout << "Enter Your Name: ";
+   std::cin >> str;
+   std::cout << std::endl << "Hello, " << str << std::endl;
+   std::cout << std::endl << std::endl;
+   return 0;
+}
